4e4605ae61db8263a5488355fb4f3c611a805beb
katcozadd <krosecozadd@gmail.com>
2018-05-22 18:17:55 -0600

    finished phase 2

--- catcollectr/main_app/views.py
+++ catcollectr/main_app/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Cat
+from .forms import CatForm
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
 def index(request):
 	cats = Cat.objects.all()
-	return render(request, 'index.html', {'cats': cats})
+	form = CatForm()
+	return render(request, 'index.html', {'cats': cats, 'form': form})
 
+def show(request, cat_id):
+	cat = Cat.objects.get(id=cat_id)
+	return render(request, 'show.html', {'cat': cat})
+
+def post_cat(request):
+	form = CatForm(request.POST)
+	if form.is_valid():
+		cat = form.save(commit = False)
+		cat.save()
+	return HttpResponseRedirect('/')
