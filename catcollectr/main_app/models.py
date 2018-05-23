f65bdfa9be8ff14fe37111cb79ed3e8afa624874
katcozadd <krosecozadd@gmail.com>
2018-05-23 08:57:00 -0600

    oops I forgot to commit

--- catcollectr/main_app/models.py
+++ catcollectr/main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,7 +7,9 @@ class Cat(models.Model):
 	name = models.CharField(max_length=100)
 	breed = models.CharField(max_length=100)
 	description = models.CharField(max_length=250)
+	likes = models.IntegerField(default=0)
 	age = models.IntegerField()
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 def __str__(self):
 	return self.name
--- catcollectr/main_app/urls.py
+++ catcollectr/main_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
 		path('', views.index, name='index'),
 		path('<int:cat_id>/', views.show, name='show'),
-		path('post_url/', views.post_cat, name='post_cat')
+		path('post_url/', views.post_cat, name='post_cat'),
+		path('user/<username>', views.profile, name='profile'),
+		path('login/', views.login_view, name="login"),
+		path('logout/', views.logout_view, name="logout"),
+		path('like_cat/', views.like_cat, name='like_cat'),
 ]
